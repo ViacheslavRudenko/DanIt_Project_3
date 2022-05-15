133099dd247c8c1dd2e7da8e30d3344284bb0c66
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-05-15 15:41:41 +0300

    filte by id

--- dist/js/filter.js
+++ dist/js/filter.js
@@ -1,9 +1,10 @@
 export default class Filter {
   showFilter() {
+    this.reset = document.getElementById("reset");
     const btnFilter = document.querySelector(".btn__filter--main");
     btnFilter.addEventListener("click", () => {
       const filterBox = document.querySelector(".filter-box");
-      console.log(filterBox);
+
       filterBox.style.display = "block";
     });
   }
@@ -12,7 +13,8 @@ export default class Filter {
     const radioBtns = document.querySelectorAll('input[name="doctor-name"]');
     this.radioBtns = radioBtns;
 
-    doctorName.addEventListener("click", function (e) {
+    doctorName.addEventListener("click", () => {
+      this.updateFilterVaule();
       for (const radioButton of radioBtns) {
         if (radioButton.checked) {
           let selectedSize = radioButton.id;
@@ -38,9 +40,10 @@ export default class Filter {
 
   showPacient() {
     this.namePacient = document.querySelector(".name-pacient__input-search");
-
     this.namePacient.addEventListener("input", () => {
+      this.updateFilterVaule();
       let pacientNameStr = this.namePacient.value;
+      this.defaultFilterPatient = this.namePacient.value;
       this.value = pacientNameStr;
       const allNamesPacient = document.querySelectorAll(".name");
       Array.from(allNamesPacient).forEach((pacient) => {
@@ -56,22 +59,73 @@ export default class Filter {
       });
     });
   }
+  getFilterByPriority(btn) {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      let dataIdArr = [];
+      let cardBox = document.querySelector(".card");
+
+      for (let i = 0; i < cardBox.children.length; i++) {
+        this.filterItem = cardBox.children[i].getAttribute("data-item");
+        if (this.filterItem) {
+          dataIdArr.push(...[this.filterItem]);
+        }
+      }
+
+      if (btn.id == "low") {
+        this.sortdataIdArr = dataIdArr.sort();
+      } else {
+        this.sortdataIdArr = dataIdArr.sort().reverse();
+      }
+
+      let flag = 0;
+
+      for (let i = 0; i < cardBox.children.length; i++) {
+        let filterItem = cardBox.children[i].getAttribute("data-item");
+
+        if (filterItem) {
+          let oldFilterItem = cardBox.children[i];
+          const filterItem = this.sortdataIdArr[flag];
+          flag++;
+
+          const replceItem = cardBox.querySelector(
+            `[data-item="${filterItem}"]`
+          );
+
+          cardBox.insertAdjacentElement("afterbegin", replceItem);
+        }
+      }
+    });
+  }
+  getLowPriority() {
+    const btn = document.querySelector(".filter__id--lower");
+    this.getFilterByPriority(btn);
+  }
+  getGrowPriority() {
+    const btn = document.querySelector(".filter__id--grow");
+    this.getFilterByPriority(btn);
+  }
 
   getDefaultFilter() {
-    const reset = document.getElementById("reset");
-    reset.addEventListener("click", () => {
+    this.reset.addEventListener("click", () => {
       this.radioBtns.forEach((e) => {
         e.click();
         e.checked = true;
       });
       this.namePacient.value = "";
+      this.reset.disabled = true;
     });
   }
 
+  updateFilterVaule() {
+    this.reset.disabled = false;
+  }
   render() {
     this.showFilter();
     this.showDoctor();
     this.showPacient();
+    this.getLowPriority();
+    this.getGrowPriority();
     this.getDefaultFilter();
   }
 }
